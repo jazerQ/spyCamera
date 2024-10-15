f4af43c25a510b6438573239a147036ae48960b2
Jazer <saltanhayrliev2006@gmail.com>
2024-10-15 22:56:22 +0300

    SpyCam 1.0.0

--- webCamera/MainWindow.xaml.cs
+++ webCamera/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Runtime.InteropServices.ComTypes;
+using System.Net;
 
 namespace webCamera
 {
@@ -23,6 +29,37 @@ namespace webCamera
         public MainWindow()
         {
             InitializeComponent();
+            bIBox.MouseDoubleClick += BIBox_MouseDoubleClick;
+            FormLoad();
+        }
+
+        private void BIBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            MessageBox.Show(string.Join("\n", host.AddressList.
+                Where(i => i.AddressFamily == AddressFamily.InterNetwork).
+                Select(i => i.ToString())));
+        }
+
+        private async void FormLoad()
+        {
+            var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+            var client = new UdpClient(port);
+
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+                BitmapImage bitmap = new BitmapImage();
+                using (var ms = new MemoryStream(data.Buffer))
+                {
+                    bitmap.BeginInit();
+                    bitmap.StreamSource = ms;
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.EndInit();
+                }
+                IBox.Source = bitmap;
+                this.Title = $"Bytes received: {data.Buffer.Length * sizeof(byte)}";
+            }
         }
     }
 }
